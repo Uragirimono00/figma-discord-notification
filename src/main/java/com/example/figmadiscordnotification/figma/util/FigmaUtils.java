5858c348fcf739ac6f5b33d4b6ff83371724330f
urgirimono00 <gudtj7578@gmail.com>
2023-09-10 22:24:01 +0900

    refactor: Discord Embed로 전송되도록 변경

--- src/main/java/com/example/figmadiscordnotification/figma/util/FigmaUtils.java
+++ src/main/java/com/example/figmadiscordnotification/figma/util/FigmaUtils.java
@@ -11,7 +11,8 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Objects;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Component
 @Slf4j
@@ -24,8 +25,10 @@ public class FigmaUtils {
     @Value("${discord.webhookUrl}")
     private String discordWebhookUrl;
     private FigmaFile previousFileState;
+    private String figmaFileId;
 
     public FigmaFile getFile(String fileId) {
+        figmaFileId = fileId;
         String url = apiUrl + "/files/" + fileId;
         RestTemplate restTemplate = new RestTemplate();
 
@@ -60,8 +63,14 @@ public class FigmaUtils {
             HttpHeaders discordHeaders = new HttpHeaders();
             discordHeaders.setContentType(MediaType.APPLICATION_JSON);
 
-            String jsonBody = "{ \"content\": \"" + message + "\" }";
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String currentDate = dateFormat.format(new Date());
 
+            String title = "Figma 파일이 변경되었습니다!";
+            String url = "https://www.figma.com/file/" + figmaFileId;
+            String jsonBody = "{ \"embeds\": [{ \"title\": \"" + title + "\", \"url\": \"" + url + "\", \"description\": \"**Updates**:" + message + "\", \"color\": 16711680, \"footer\": {\"text\": \"" + currentDate + "\"} }] }";
+
+            log.info(jsonBody);
             HttpEntity<String> discordEntity = new HttpEntity<>(jsonBody, discordHeaders);
 
             ClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
@@ -70,7 +79,7 @@ public class FigmaUtils {
             ResponseEntity<String> discordResponse = restTemplate.exchange(discordWebhookUrl, HttpMethod.POST, discordEntity, String.class);
 
             if (discordResponse.getStatusCode() != HttpStatus.OK) {
-//                throw new RuntimeException("Failed to send message to Discord Webhook");
+//                throw new RuntimeException("디스코드 웹훅 전송을 실패하였습니다.");
             }
         } catch (Exception e) {
             throw new RuntimeException("디스코드 웹훅 전송을 실패하였습니다. \n", e);
@@ -79,7 +88,6 @@ public class FigmaUtils {
 
     public void compareChildrenLevels(Node previousNode, Node currentNode, int level) {
         if (previousNode == null || currentNode == null) {
-            // 이전 또는 현재 노드가 없는 경우 처리할 필요가 없습니다.
             return;
         }
 
@@ -91,15 +99,15 @@ public class FigmaUtils {
         // 2번째 뎁스 변경 사항 검사
         if (level == 1) {
             int childrenDiff = currentChildrenCount - previousChildrenCount;
-            String message = levelPrefix + "2번째 뎁스 " + "의 children 개수 변경: ";
+            String message = "";
             if (childrenDiff > 0) {
-                message += childrenDiff + "개 추가";
+                message += "\\n✅ " + levelPrefix + " 2번째 뎁스 " + childrenDiff + "개 추가";
             } else if (childrenDiff < 0) {
-                message += (-childrenDiff) + "개 삭제";
+                message += "\\n❌ " + levelPrefix + " 2번째 뎁스 " +  (-childrenDiff) + "개 삭제";
             }
             if (childrenDiff != 0) { // 변경이 있을 때만 로그 출력
-                log.info(message);
-//                sendNotificationToDiscord(message);
+//                log.info(message);
+                sendNotificationToDiscord(message);
             }
         }
 
@@ -109,18 +117,17 @@ public class FigmaUtils {
             int currentThirdLevelChildrenCount = currentNode.getChildren().size();
             int thirdLevelChildrenDiff = currentThirdLevelChildrenCount - previousThirdLevelChildrenCount;
 
-            // 3번째 뎁스의 변경된 요소 개수만 로그 출력
             if (level == 2 && thirdLevelChildrenDiff != 0) {
-                String message = currentNode.getName() + "의 " + levelPrefix + "3번째 뎁스의 변경된 요소 개수: " + thirdLevelChildrenDiff;
-                log.info(message);
-//                sendNotificationToDiscord(message);
+                String message = "\\n:pencil2:" + currentNode.getName() + "의 " + levelPrefix + "3번째 뎁스의 변경된 요소 개수: " + thirdLevelChildrenDiff;
+//                log.info(message);
+                sendNotificationToDiscord(message);
             }
 
             for (int i = 0; i < Math.min(previousChildrenCount, currentChildrenCount); i++) {
                 Node previousThirdLevelNode = previousNode.getChildren().get(i);
                 Node currentThirdLevelNode = currentNode.getChildren().get(i);
 
-                // 재귀적으로 더 깊은 뎁스 비교
+                // 재귀돌려서 더 깊은 뎁스 비교
                 compareChildrenLevels(previousThirdLevelNode, currentThirdLevelNode, level + 1);
             }
         }
